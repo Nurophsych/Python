ff94f49c249d8b46eca9e4fd60f42480682147e0
Nurophsych <kvanstraaten02@gmail.com>
2025-01-14 19:21:29 +0200

    Delete Chapter_1 directory

--- Chapter_1/Pg_71/Famous_quote.py
+++
@@ -1,4 +0,0 @@
-name = 'Madonna'
-message = f"{name.capitalize()} once said, " 
-msgpt2 = "\"Listen, everyone is entitled to my opinion.\""
-print(message, msgpt2)
--- Chapter_1/Pg_71/Name_cases.py
+++
@@ -1,4 +0,0 @@
-name = 'Madonna'
-message = "%s once said, " 
-msgpt2 = "\"Listen, everyone is entitled to my opinion\""
-print(message.title(), msgpt2.title % name.title())
