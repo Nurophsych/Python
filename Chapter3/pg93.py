d5fbbc8818fc97e63c6d27ea71c86d8c608b9cc5
Keenan <kvanstraaten02@gmail.com>
2025-01-22 18:00:28 +0200

    Add files via upload

---
+++ Chapter3/pg93.py
@@ -0,0 +1,17 @@
+contries = ['Japan','Chile','Germany','Ireland','Greece']
+print(contries)
+
+newlis = sorted(contries)
+print(newlis)
+print(contries)
+print(contries)
+newlis = sorted(contries, reverse = True)
+print(newlis)
+print(contries)
+newlis = contries.reverse()
+print(newlis)
+print(contries)
+newlis = contries.reverse()
+print(newlis)
+print(contries)
+
