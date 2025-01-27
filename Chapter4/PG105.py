5d98544aa15c0efff191916b16f23071435a6402
Keenan <kvanstraaten02@gmail.com>
2025-01-27 18:16:56 +0200

    Add files via upload

---
+++ Chapter4/PG105.py
@@ -0,0 +1,12 @@
+lisstOfFavortitePizza = ['mexicana', 'somt hing meaty', 'triple deccker']
+for i in lisstOfFavortitePizza:
+    print(f"I like {i}")
+print("OH MY F#!8ING GOD I LOVE {i} PIZZA!!!!!!!!!!")
+
+
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    #4-2
+listoanimals = ['wolf','fox','cyote']
+for i in listoanimals:
+    print(f"A {i} would make a greate pet")
+print("the similarity for these bois is their lil pawsies!!!!!!!!!!")
